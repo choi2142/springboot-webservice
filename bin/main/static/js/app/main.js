1cc4bee2209b06581ec87ef2f649587103cb2d73
200211 <200211@DESKTOP-BSPULKD>
2020-03-10 17:11:47 +0900

    수정페이지 구현중

--- bin/main/static/js/app/main.js
+++ bin/main/static/js/app/main.js
@@ -4,6 +4,12 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+        $('#btn-detail').on('click', function () {
+            _this.detail();
+        });
     },
     save : function () {
         var data = {
@@ -20,6 +26,48 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
+            location.reload();
+        }).fail(function (error) {
+            alert(error);
+        });
+    },
+    update : function () {
+        var data2 = {
+        	id: $('#id').val(),
+            title: $('#title2').val(),
+            author: $('#author2').val(),
+            content: $('#content2').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/postsupdate',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data2: JSON.stringify(data2)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            location.reload();
+        }).fail(function (error) {
+            alert(error);
+        });
+    }
+    ,
+    detail : function () {
+        var data3 = {
+        	id: $('#list_id').val(),
+            title: $('#btn-detail').val(),
+            author: $('#list_author').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/postsupdate',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data2: JSON.stringify(data3)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
             location.reload();
         }).fail(function (error) {
             alert(error);
