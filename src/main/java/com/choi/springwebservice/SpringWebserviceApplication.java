bdd305af60a45b811ed11a9bb55298646a22e0a3
200211 <200211@DESKTOP-BSPULKD>
2020-03-13 10:27:48 +0900

    init

--- src/main/java/com/choi/springwebservice/SpringWebserviceApplication.java
+++ src/main/java/com/choi/springwebservice/SpringWebserviceApplication.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class SpringWebserviceApplication {
 	
-	 public static final String APPLICATION_LOCATIONS = "spring.config.location="
-	            + "classpath:application.yml,"
-	            + "classpath:../config/real-application.yml";
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
 
 	    public static void main(String[] args) {
 	        new SpringApplicationBuilder(SpringWebserviceApplication.class)
